89e44e006465013a509f838321a66fe4136f414f
Klizlo <klizlo0111@gmail.com>
2023-10-10 19:06:12 +0200

    Modify CardView

--- MemoryGame/CardView.swift
+++ MemoryGame/CardView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct CardView: View {
+    @State var isHiden = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 12)
+            .fill(isHiden ? Color.blue : Color.white)
+            .border(Color.blue, width: 2)
+            .font(.largeTitle)
     }
 }
 
